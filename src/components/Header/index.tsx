afa76e9855b82df1066aaaecae0776b823894d35
Tsherculano <tico.herculano@gmail.com>
2022-07-23 13:59:17 -0300

    feat: Created Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import logoImg from '../../assets/logo.svg';
-import * as S from '../styles';
 
-export function Header() {
+import * as S from './styles';
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal}:HeaderProps) {
+
   return (
     <S.Container>
       <S.Content>
       <img src={logoImg} alt="dtmoney" />
-      <button type="button">
+      <button type="button" onClick={onOpenNewTransactionModal}>
         Nova tansação
       </button>
+
+
       </S.Content>
     </S.Container>
   )
